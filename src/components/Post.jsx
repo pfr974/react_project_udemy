0f124670ec177d97675b5fbd57faf42097e3a4a0
Fred <pf.robert974@gmail.com>
2024-02-22 16:52:39 +1100

    Name in Post component not hardcodded anymore

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,7 +1,11 @@
+const names = ["Frederic", "Paul"];
+
 function Post() {
+  const chosenName = Math.random() > 0.5 ? names[0] : names[1];
+
   return (
     <div>
-      <p>Frederic</p>
+      <p>{chosenName}</p>
       <p>I am learning about React Components!</p>
     </div>
   );
