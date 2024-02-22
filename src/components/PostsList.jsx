566a5d7a0b360b4e19af4112fae5a564e909473b
Fred <pf.robert974@gmail.com>
2024-02-22 19:41:31 +1100

    overlay Modal can visible or not

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,11 +1,17 @@
 import Post from "./Post";
 import NewPost from "./NewPost";
+import Modal from "./Modal";
 import classes from "./PostsList.module.css";
 import { useState } from "react";
 
 function PostsList() {
+  const [modalIsVisible, setModalIsVisible] = useState(true);
   const [enteredBody, setEnteredBody] = useState("");
   const [enteredAuthor, setEnteredAuthor] = useState("");
+
+  function hideModalHandler() {
+    setModalIsVisible(false);
+  }
 
   function bodyChangeHandler(event) {
     setEnteredBody(event.target.value);
@@ -15,12 +21,22 @@ function PostsList() {
     setEnteredAuthor(event.target.value);
   }
 
+  let modalContent;
+
+  if (modalIsVisible) {
+    modalContent = (
+      <Modal onClose={hideModalHandler}>
+        <NewPost
+          onBodyChange={bodyChangeHandler}
+          onAuthorChange={authorChangeHandler}
+        />
+      </Modal>
+    );
+  }
+
   return (
     <>
-      <NewPost
-        onBodyChange={bodyChangeHandler}
-        onAuthorChange={authorChangeHandler}
-      />
+      {modalContent}
       <ul className={classes.posts}>
         <Post author={enteredAuthor} body={enteredBody} />
         <Post author="Paul" body="No kidding!" />
