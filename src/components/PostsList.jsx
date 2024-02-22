23f2db1410394ee560ad160d305bd85967abf893
Fred <pf.robert974@gmail.com>
2024-02-22 17:33:47 +1100

    NewPost component

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,12 +1,19 @@
 import Post from "./Post";
+import NewPost from "./NewPost";
 import classes from "./PostsList.module.css";
 
 function PostsList() {
   return (
-    <ul className={classes.posts}>
-      <Post author="Fred" body="I am learning about React components •̀.̫ •́✧!" />
-      <Post author="Paul" body="No kidding!" />
-    </ul>
+    <>
+      <NewPost />
+      <ul className={classes.posts}>
+        <Post
+          author="Fred"
+          body="I am learning about React components •̀.̫ •́✧!"
+        />
+        <Post author="Paul" body="No kidding!" />
+      </ul>
+    </>
   );
 }
 
