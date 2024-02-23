bb9ef32b9f56d68cb88dfc1d73e0a1e1345e8372
Fred <pf.robert974@gmail.com>
2024-02-23 11:21:39 +1100

    added Cancel button

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -25,6 +25,7 @@ function PostsList({isPosting, onStopPosting}) {
         <NewPost
           onBodyChange={bodyChangeHandler}
           onAuthorChange={authorChangeHandler}
+          onCancel={onStopPosting}
         />
       </Modal>
     );
